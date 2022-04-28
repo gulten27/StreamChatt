590048bd7a1e4e89654af44495ff5bcd0ea878c4
gulten27 <ggdogann00@gmail.com>
2022-04-28 21:05:48 +0300

    chat

---
+++ app/src/main/java/com/gultendogan/streamchatapp/ui/BindingFragment.kt
@@ -0,0 +1,30 @@
+package com.gultendogan.streamchatapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.gultendogan.streamchatapp.R
+
+
+abstract class BindingFragment<out T : ViewBinding> : Fragment() {
+
+    private var _binding: ViewBinding? = null
+    @Suppress("UNCHECKED_CAST")
+    protected val binding: T
+        get() = _binding as T
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = bindingInflater(inflater)
+        return _binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    protected abstract val bindingInflater: (LayoutInflater) -> ViewBinding
+}
---
+++ app/src/main/java/com/gultendogan/streamchatapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.gultendogan.streamchatapp.util
+
+object Constants {
+
+    const val MIN_USERNAME_LENGTH = 3
+}
